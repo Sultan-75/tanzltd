ae2c2a2c6f1f4a479e13f26e3d9b1eeeb89b6932
sultan ahmed <sultan7.syl@gmail.com>
2022-09-08 02:03:49 +0600

    crete services page & setup home paGE

--- src/components/Pricing/Pricing.elements.js
+++ src/components/Pricing/Pricing.elements.js
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const PricingSection = styled.div`
   padding: 100px 0 160px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background: #4b59f7;
 `;
 
 export const PricingWrapper = styled.div`
@@ -24,13 +23,14 @@ export const PricingWrapper = styled.div`
 `;
 
 export const PricingHeading = styled.h1`
-  color: #fff;
+  color: #0a1435;
   font-size: 48px;
   margin-bottom: 24px;
 `;
 
 export const PricingContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
 
@@ -44,16 +44,13 @@ export const PricingContainer = styled.div`
 `;
 
 export const PricingCard = styled(Link)`
-  background: #242424;
+  background: #0a1435;
   box-shadow: 0 6px 20px rgba(56, 125, 255, 0.2);
   width: 280px;
-  height: 500px;
+  height: 270px;
   text-decoration: none;
   border-radius: 4px;
-
-  &:nth-child(2) {
-    margin: 24px;
-  }
+  margin: 24px;
 
   &:hover {
     transform: scale(1.06);
@@ -71,41 +68,16 @@ export const PricingCard = styled(Link)`
 `;
 
 export const PricingCardInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 500px;
-  padding: 24px;
-  align-items: center;
+  text-align: center;
   color: #fff;
-`;
-
-export const PricingCardIcon = styled.div`
-  margin: 24px 0;
+  .card-img {
+    max-width: 270px;
+    max-height: auto;
+  }
 `;
 
 export const PricingCardPlan = styled.h3`
   margin-bottom: 5px;
+  margin-top: 15px;
   font-size: 24px;
-`;
-
-export const PricingCardCost = styled.h4`
-  font-size: 40px;
-`;
-
-export const PricingCardLength = styled.p`
-  font-size: 14px;
-  margin-bottom: 24px;
-`;
-
-export const PricingCardFeatures = styled.ul`
-  margin: 16px 0 32px;
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: #a9b3c1;
-`;
-
-export const PricingCardFeature = styled.li`
-  margin-bottom: 10px;
 `;
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
-import { InfoSection, Pricing } from '../../components';
+import React from "react";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
+import { InfoSection, Pricing } from "../../components";
 
 function Home() {
   return (
     <>
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjThree} />
-      <InfoSection {...homeObjTwo} />
+
       <Pricing />
       <InfoSection {...homeObjFour} />
     </>
